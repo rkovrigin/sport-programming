517cc9267195531653337b5250f3c066b32a1178
Roman Kovrigin <romakovrigin@gmail.com>
2019-09-11 14:23:04 +0200

    Add print tast 1

--- 1-1-perebor_posledovatelnostey.py
+++ 1-1-perebor_posledovatelnostey.py
@@ -1,17 +1,17 @@
-n = 6
-m = 5
+n = 4
+m = 2
 
 a = [0] * n
 index = 1
-out_index = 6659
+out_index = 3
 
 def rec(idx):
     global index
     if idx == n:
         if index == out_index:
-            print a, index
+            print (a, index)
         index += 1
-        # print(index)
+        print(a)
         return
     for i in range(1, m + 1):
         a[idx] = i
