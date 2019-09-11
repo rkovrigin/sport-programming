e091a97b625db29bac340ea01d79ea0418d9469a
Roman Kovrigin <romakovrigin@gmail.com>
2019-09-11 07:43:16 +0200

    Fix 1-6

--- 1-6-komivoyajor.py
+++ 1-6-komivoyajor.py
@@ -4,8 +4,8 @@ a = [[0,1,4,6],
      [4,5,0,3],
      [6,2,3,0],]
 
-used = [False] * n
-p = [0] * n
+used = [False] * (n)
+p = [0] * (n)
 ans = 10000
 
 def rec(idx, len):
@@ -13,14 +13,15 @@ def rec(idx, len):
     if len >= ans:
         return
     if idx == n:
-        ans = min(ans, len + a[p[idx - 1]][0])
-    for i in range(n):
+        ans = min(ans, len + a[p[idx-1]][0])
+        return
+    for i in range(1, n):
         if used[i]:
             continue
         p[idx] = i
-        used[idx] = True
-        rec(idx + 1, len + a[p[idx] - 1][i])
-        used[idx] = False
-used[0] = True
+        used[i] = True
+        rec(idx + 1, len + a[p[idx-1]][i])
+        used[i] = False
+
+p[0] = 0
 rec(1, 0)
-print(ans)
