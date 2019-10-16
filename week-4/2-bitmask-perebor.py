892a1b6c87cd072245793f36428e23fe850f2eea
Roman Kovrigin <romakovrigin@gmail.com>
2019-10-16 08:39:47 +0200

    Lexograph sequence

--- week-4/2-bitmask-perebor.py
+++ week-4/2-bitmask-perebor.py
@@ -5,4 +5,13 @@ def mask_perebor(n):
                 print i+1,
         print ''
 
+def mask_perebor_lexograph(n):
+    for mask in range(1 << n)[::-1]:
+        for i in range(n, -1, -1):
+            if mask & (1 << i):
+                print n-i,
+        print ''
+
 mask_perebor(3)
+print "---"
+mask_perebor_lexograph(3)
