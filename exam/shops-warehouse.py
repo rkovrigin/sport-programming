105817103f07ec15365ec712fad125e77017c553
Roman Kovrigin <romakovrigin@gmail.com>
2019-12-10 19:14:03 +0100

    exam shops

--- exam/shops-warehouse.py
+++ exam/shops-warehouse.py
@@ -4,10 +4,20 @@ warehouse = [-488, 389, 138, 392, -398, -38, 16, 72, -456, -196]
 # shop = [3, 5]
 # warehouse = [6, 2]
 
+with open('shops2.in', 'r') as f:
+    _ = f.readline()
+    shop = [int(i) for i in f.readline().split()]
+    warehouse = [int(i) for i in f.readline().split()]
+
+shop.sort(reverse=True)
+warehouse.sort(reverse=True)
+
+print(sum([abs(i - j) for i,j in zip(shop, warehouse)]))
+exit()
 out = []
 for s in shop:
-    tmp = 10000000
-    _w = 10000000
+    tmp = abs(s - warehouse[0])
+    _w = warehouse[0]
     for w in warehouse:
         if abs(s - w) < tmp:
             tmp = abs(s - w)
